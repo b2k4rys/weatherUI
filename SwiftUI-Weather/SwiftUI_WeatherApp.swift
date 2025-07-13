496c91ae90c8d1e9a1a6963224613c0bae692f44
Bekarys <bekarys20050812@gmail.com>
2025-07-13 17:48:03 +0500

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Bekarys Maksutbek on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
